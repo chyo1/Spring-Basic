a7b6c16b6d8dddab7ba753e669d4983d9f892a37
chyo1 <hyowon3856@gmail.com>
2025-03-06 16:41:44 +0900

    Chapter 8

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,30 +15,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    /*
-        할인 정책이 변경되면, 클라이언트 코드인 OrderServiceImpl 코드의 변경이 필요함 -> DIP 위반
-        //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-        //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-     */
-
-    /*
-        그럼 인터페이스에만 의존하게 변경? -> NPE 발생
-        //    private final DiscountPolicy discountPolicy;
-     */
-
-    /*
-        인터페이스에만 의존할 수 있게 생성자 주입으로 변경
-     */
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     // 스프링 컨테이너가 자동으로 해당 스프린 빈을 찾아 주입, 여러 의존 관계도 한 번에 주입 가능
     // 기본 조회 전략은 타입이 같은 빈을 찾아서 주입
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
